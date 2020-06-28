bd8426eacd2432f5deabab72670ea309229f8725
harshasukhdeve <harshasukhdeve0903@gmail.com>
2020-06-28 20:43:00 +0530

    FSJ0420A115

---
+++ lab-angular-services-movies-hall-of-fames/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ImagesService } from './images.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'lab-angular-services-movies-hall-of-fame';
+
+  image: any = [];
+  constructor(private imageservice: ImagesService) { }
+
+  ngOnInit(): void {
+  }
+  getImage(){
+   this.image = this.imageservice.getImage();
+   console.log(this.image);
+}
+}
